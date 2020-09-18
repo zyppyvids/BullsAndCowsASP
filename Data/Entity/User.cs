7bb94de766643dbd421ea102a41aac482d7f56a2
Emil Yordanov <zyppyvids@gmail.com>
2020-09-19 02:44:33 +0300

    Added User model

---
+++ Data/Entity/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Data.Entity
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Username is required")]
+        [MaxLength(80, ErrorMessage = "Username cannot be longer than 80 characters")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        [StringLength(18, ErrorMessage = "The password must be at least 6 characters", MinimumLength = 6)]
+        [RegularExpression(@"^((?=.*[a-z])(?=.*[A-Z])(?=.*\d)).+$")]
+        [DataType(DataType.Password)]
+        public string PasswordHash { get; set; }
+
+        public int GamesWon { get; set; }
+    }
+}
