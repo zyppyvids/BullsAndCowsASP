4de82aa9369415659a760102be3a2f8b72326de2
Emil Yordanov <zyppyvids@gmail.com>
2020-09-19 02:44:56 +0300

    Added Cookie and Session Extensions used to ensure security

---
+++ Web/Controllers/Extensions/CookieExtensions.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Http;
+
+public static class CookieExtensions
+{
+    public static string GetCookie(HttpRequest request, string key)
+    {
+        try
+        {
+            return request.Cookies[key];
+        }
+        catch (KeyNotFoundException)
+        {
+            return "0";
+        }
+    }
+
+    public static void SetCookie(HttpResponse response, string key, string value, int? expireTime = null)
+    {
+        CookieOptions option = new CookieOptions();
+
+        if (expireTime.HasValue)
+        {
+            option.Expires = DateTime.Now.AddMinutes(expireTime.Value);
+
+            response.Cookies.Append(key, value, option);
+        }
+        else
+        {
+            response.Cookies.Append(key, value);
+        }
+    }
+
+    public static void RemoveCookie(HttpResponse response, string key)
+    {
+        response.Cookies.Delete(key);
+    }
+}
