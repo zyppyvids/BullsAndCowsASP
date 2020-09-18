7e3f921a18da75f1d0d34e4790f65aa5aee0a1a9
Emil Yordanov <zyppyvids@gmail.com>
2020-09-19 02:44:25 +0300

    Added DbContext class

---
+++ Data/MyGameDb.cs
@@ -0,0 +1,21 @@
+﻿using Data.Entity;
+using Microsoft.EntityFrameworkCore;
+
+namespace Data
+{
+    public class MyGameDb : DbContext
+    {
+        public virtual DbSet<User> Users { get; set; }
+
+
+        public MyGameDb()
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(@"Data Source=gamedb.db");
+        }
+    }
+}
